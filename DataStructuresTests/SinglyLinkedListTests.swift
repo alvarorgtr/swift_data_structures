fbef6301b255edeb41daad74119a9246526535d0
Alvaro Rodriguez Garcia <alvaro@deltaapps.net>
2016-06-29 15:24:12 +0200

    Added empty test files

---
+++ DataStructuresTests/SinglyLinkedListTests.swift
@@ -0,0 +1,25 @@
+//
+//  ForwardListTests.swift
+//  DataStructures
+//
+//  Created by Álvaro Rodríguez García on 29/6/16.
+//  Copyright © 2016 DeltaApps. All rights reserved.
+//
+
+import XCTest
+@testable import DataStructures
+
+class SinglyLinkedListTests: XCTestCase {
+	
+	override func setUp() {
+		super.setUp()
+		// Put setup code here. This method is called before the invocation of each test method in the class.
+	}
+	
+	override func tearDown() {
+		// Put teardown code here. This method is called after the invocation of each test method in the class.
+		super.tearDown()
+	}
+	
+	
+}
